de30d71905839ce2b1c7e8bcc998fdcb3b6bee24
Jean Dos Santos <jeandsantos88@gmail.com>
2024-12-28 18:41:33 +0100

    Edits to virtual proxy example

--- design_patterns/structural/proxy/virtual_proxy.py
+++ design_patterns/structural/proxy/virtual_proxy.py
@@ -37,19 +37,19 @@ def main():
 
     print()
 
-    lazy_bitmap = LazyBitmap("lazy_image.png")
+    lazy_bitmap = LazyBitmap("image.png")
     draw_image(lazy_bitmap)
 
 
 if __name__ == "__main__":
     main()
     # output:
-    # Loading image from image.png       <-- Image is eagerly loaded
+    # Loading image from image.png      <-- Image is eagerly loaded
     # About to draw image
     # Drawing image from image.png
     # Done drawing image
     #
     # About to draw image
-    # Loading image from lazy_image.png  <-- Image is only loaded when it is needed
-    # Drawing image from lazy_image.png
+    # Loading image from image.png      <-- Image is only loaded when it is needed
+    # Drawing image from image.png
     # Done drawing image
