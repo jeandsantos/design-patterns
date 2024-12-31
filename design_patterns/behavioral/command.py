1f9c4e09984980c2ad25d24d66f9cbee66aa7a0f
Jean Dos Santos <jeandsantos88@gmail.com>
2024-12-31 18:12:35 +0100

    Refactored command.py

--- design_patterns/behavioral/command.py
+++ design_patterns/behavioral/command.py
@@ -1,17 +1,11 @@
 """Bank account with command pattern."""
 
 from abc import ABC, abstractmethod
-from enum import Enum, auto
 
-
-class Actions(Enum):
-    DEPOSIT = auto()
-    WITHDRAW = auto()
+OVERDRAFT_LIMIT: int = -500
 
 
 class BankAccount:
-    OVERDRAFT_LIMIT: int = -500
-
     def __init__(self, balance: int = 0):
         self.balance = balance
 
@@ -20,7 +14,7 @@ class BankAccount:
         print(f"Deposited {amount}. Current balance: {self.balance}")
 
     def withdraw(self, amount: int) -> bool:
-        if self.balance - amount < BankAccount.OVERDRAFT_LIMIT:
+        if self.balance - amount < OVERDRAFT_LIMIT:
             print("Insufficient funds")
             return False
         self.balance -= amount
@@ -38,53 +32,65 @@ class Command(ABC):
     def undo(self): ...
 
 
-class BankAccountCommand(Command):
-    def __init__(self, account: BankAccount, action: Actions, amount: int):
+class WithdrawCommand(Command):
+    def __init__(self, account: BankAccount, amount: int):
         self.account = account
-        self.action = action
         self.amount = amount
         self.success: bool | None = None
 
     def invoke(self):
-        if self.action == Actions.DEPOSIT:
-            self.account.deposit(self.amount)
-            self.success = True
-        elif self.action == Actions.WITHDRAW:
-            self.success = self.account.withdraw(self.amount)
-        else:
-            raise ValueError(f"Unknown action: {self.action}")
+        self.success = self.account.withdraw(self.amount)
 
     def undo(self):
         if not self.success:
             return
 
-        if self.action == Actions.DEPOSIT:
-            self.account.withdraw(self.amount)
-        elif self.action == Actions.WITHDRAW:
-            self.account.deposit(self.amount)
-        else:
-            raise ValueError(f"Unknown action: {self.action}")
+        self.account.deposit(self.amount)
+
+
+class DepositCommand(Command):
+    def __init__(self, account: BankAccount, amount: int):
+        self.account = account
+        self.amount = amount
+        self.success: bool | None = None
+
+    def invoke(self):
+        self.account.deposit(self.amount)
+        self.success = True
+
+    def undo(self):
+        if not self.success:
+            return
+        self.account.withdraw(self.amount)
 
 
 def main():
-    bank_account = BankAccount()
-    print(bank_account)
-    print()
+    bank_account = BankAccount(0)
+    print(bank_account, "\n")
 
-    deposit_command = BankAccountCommand(bank_account, Actions.DEPOSIT, 1000)
+    deposit_command = DepositCommand(bank_account, 1000)
     deposit_command.invoke()
     print(bank_account)
     deposit_command.undo()
-    print(bank_account)
+    print(bank_account, "\n")
 
-    print()
-
-    illegal_command = BankAccountCommand(bank_account, Actions.WITHDRAW, 1000)
-    illegal_command.invoke()
+    unauthorized_withdrawal_command = WithdrawCommand(bank_account, 1000)
+    unauthorized_withdrawal_command.invoke()
     print(bank_account)
-    illegal_command.undo()
+    unauthorized_withdrawal_command.undo()
     print(bank_account)
 
 
 if __name__ == "__main__":
     main()
+    # Output:
+    # Balance: 0
+    #
+    # Deposited 1000. Current balance: 1000
+    # Balance: 1000
+    # Withdrew 1000. Current balance: 0
+    # Balance: 0
+    #
+    # Insufficient funds
+    # Balance: 0
+    # Balance: 0
